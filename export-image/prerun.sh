5fba8f30c37e93b3e87da5895f584849ccca14d9
rreichenbach <randaji@gmail.com>
2026-04-09 12:44:02 -0700

    a more deterministic calc of ROOT_SIZE and ROOT_MARGIN

--- export-image/prerun.sh
+++ export-image/prerun.sh
@@ -10,7 +10,12 @@ rm -rf "${ROOTFS_DIR}"
 mkdir -p "${ROOTFS_DIR}"
 
 BOOT_SIZE="$((512 * 1024 * 1024))"
-ROOT_SIZE=$(du -x --apparent-size -s "${EXPORT_ROOTFS_DIR}" --exclude var/cache/apt/archives --exclude boot/firmware --block-size=1 | cut -f 1)
+
+ROOT_SIZE=$(du -x -s "${EXPORT_ROOTFS_DIR}" \
+  --exclude var/cache/apt/archives \
+  --exclude boot/firmware \
+  --block-size=1 | cut -f 1)
+echo "ROOT_SIZE: $ROOT_SIZE"
 
 # All partition sizes and starts will be aligned to this size
 ALIGN="$((4 * 1024 * 1024))"
@@ -18,7 +23,7 @@ ALIGN="$((4 * 1024 * 1024))"
 # some overhead (since actual space usage is usually rounded up to the
 # filesystem block size) and gives some free space on the resulting
 # image.
-ROOT_MARGIN="$(echo "($ROOT_SIZE * 0.2 + 500 * 1024 * 1024) / 1" | bc)"
+ROOT_MARGIN=$((700 * 1024 * 1024))
 
 BOOT_PART_START=$((ALIGN))
 BOOT_PART_SIZE=$(((BOOT_SIZE + ALIGN - 1) / ALIGN * ALIGN))
